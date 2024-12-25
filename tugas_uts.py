0a45df05a14825a855eaf051f0120e18a11a72ad
rufi1126 <rufijr67@gmail.com>
2024-12-25 14:55:40 +0700

    Update tugas_uts.py

--- tugas_uts.py
+++ tugas_uts.py
@@ -1,10 +1,11 @@
+#buatlah program untuk fungsi menghitung perkalian matrix pada matrix 4x4 untuk matrix berikut : di ketahui 
 # Matriks C
 C = np.array([
     [44, 55, 54, 10],
     [33, 5, 32, 13],
     [21, 7, 21, 11],
     [33, 88, 32, 15]
-])
+
 
 # Matriks D
 D = np.array([
@@ -12,7 +13,7 @@ D = np.array([
     [7, 1, 0, 7],
     [2, 0, 1, 2],
     [1, 3, 1, 2]
-])
+
 
 # Perkalian matriks C x D
 result = np.dot(C, D)
